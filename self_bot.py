0a17bbe9105ec6edb6168d1137edabaecb988b61
wfsec <69239087+wfsecs@users.noreply.github.com>
2023-01-13 23:43:08 +0200

    Catreply fix
    Catreply wont respond to bots

--- self_bot.py
+++ self_bot.py
@@ -95,8 +95,9 @@ async def on_message_delete(message):
 @bot.listen()
 async def on_message(message):
     if catreply:
-        if message.author != bot.user:
-            await message.reply(random.choice(catgifs), mention_author=True)
+        if not message.author.bot:
+            if message.author != bot.user:
+                await message.reply(random.choice(catgifs), mention_author=True)
 
     if grouplock:
         if message.channel.id == grouplock_group:
